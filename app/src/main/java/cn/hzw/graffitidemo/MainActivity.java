38fd2d2856107987b8e8315da0d6f9b355cb4e46
hzw <1993hzw@163.com>
2016-08-31 01:12:51 +0800

    修改了gradle配置

--- app/src/main/java/cn/hzw/graffitidemo/MainActivity.java
+++ app/src/main/java/cn/hzw/graffitidemo/MainActivity.java
@@ -1,9 +1,9 @@
 package cn.hzw.graffitidemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
