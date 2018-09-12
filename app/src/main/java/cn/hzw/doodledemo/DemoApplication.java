405527c9027a2e58deef7e0f9970fa5137e0e036
huangziwei <huangziwei1@bigo.sg>
2018-09-12 17:46:02 +0800

    Rename demo

---
+++ app/src/main/java/cn/hzw/doodledemo/DemoApplication.java
@@ -0,0 +1,10 @@
+package cn.hzw.doodledemo;
+
+import android.app.Application;
+
+public class DemoApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
